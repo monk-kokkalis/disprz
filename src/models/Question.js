db7b7a7a67ef94f598ac092b24f0b1939fae2284
Will Yngson <christianwillyngson@gmail.com>
2021-12-29 19:57:42 +0800

    added uuid for unique question ids

--- src/models/Question.js
+++ src/models/Question.js
@@ -1,5 +1,7 @@
+import { v4 as uuidv4 } from 'uuid';
 class Question {
     constructor({name, sequenceNumber, active}) {
+        this.id = `${Date.now()}${uuidv4()}`;
         this.active = active;
         this.name = name;
         this.sequenceNumber = sequenceNumber;
