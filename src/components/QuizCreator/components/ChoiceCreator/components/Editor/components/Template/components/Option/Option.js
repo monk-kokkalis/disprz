c1da0c6a6c08202d723c3dadf466bedd7c3f3bbe
Will Yngson <christianwillyngson@gmail.com>
2022-01-01 18:00:59 +0800

    added snackbar notice to Delete option

--- src/components/QuizCreator/components/ChoiceCreator/components/Editor/components/Template/components/Option/Option.js
+++ src/components/QuizCreator/components/ChoiceCreator/components/Editor/components/Template/components/Option/Option.js
@@ -7,16 +7,24 @@ import constants from 'auxiliary/constants';
 // provider
 import {Context} from 'components/QuizCreator/context/Provider';
 import {questionActions} from 'components/QuizCreator/context/reducers/questions/index'
+// Snackbar
+import {SnackbarContext} from 'components/Snackbar/SnackbarProvider';
 function Option({option, index, choiceCreatorMode, targetQuestionIndex}) {
     const context = useContext(Context);
-    const {questDispatch} = context;
+    const {questDispatch, questState} = context;
+    const snackbar = useContext(SnackbarContext);
     const targetOptionIndex = index;
+    const targetQuestion = questState.questions[targetQuestionIndex];
     const clickHandler = () => {
         if (choiceCreatorMode === constants.DELETE) {
-            questDispatch({
-                type: questionActions.DELETE_OPTION,
-                payload: {targetQuestionIndex, targetOptionIndex}
-            });
+            if (targetQuestion?.options.length > 2) {
+                questDispatch({
+                    type: questionActions.DELETE_OPTION,
+                    payload: {targetQuestionIndex, targetOptionIndex}
+                });
+            } else {
+                snackbar.show({text: 'Unable to delete option. Questions must have atleast 2 options'});
+            }
         }
     }
     return (
