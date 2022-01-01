e2594576d1e61fc80e8912775631587829dbf731
Will Yngson <christianwillyngson@gmail.com>
2022-01-01 16:23:09 +0800

    added DeleteOption component

--- src/components/QuizCreator/components/ChoiceCreator/components/DeleteOption/DeleteOption.js
+++ src/components/QuizCreator/components/ChoiceCreator/components/DeleteOption/DeleteOption.js
@@ -1,6 +1,31 @@
+import {useContext} from 'react';
 import Style from './DeleteOption.module.scss';
+// auxiliary
+import constants from 'auxiliary/constants';
+import className from 'auxiliary/class-name';
+// provider
+import {Context} from '../../../../context/Provider';
+import {questionActions} from '../../../../context/reducers/questions/index';
 function DeleteOption() {
-    return <button className={Style.Main}>Delete</button>
+    const context = useContext(Context);
+    const {questState, questDispatch} = context;
+    const currentMode = questState.choiceCreatorMode;
+    const choiceCreatorMode = currentMode  === constants.DELETE ? constants.CREATIVE : constants.DELETE;
+    const clickHandler = () => {
+        questDispatch({
+            type: questionActions.CHANGE_CHOICE_CREATOR_MODE,
+            payload: {choiceCreatorMode}
+        });
+    }
+
+    return (
+        <button
+            className={className(Style.Main, {delete: currentMode === constants.DELETE})}
+            onClick={clickHandler}>
+                
+            Delete
+        </button>
+    )
 }
 
 export default DeleteOption;
